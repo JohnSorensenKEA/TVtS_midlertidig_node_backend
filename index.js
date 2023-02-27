9a8839d6aa0233518361662a9ade71a18fa57472
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-02-27 10:48:10 +0100

    inserts correct format and updates relations

--- index.js
+++ index.js
@@ -129,23 +129,47 @@ app.post("/here", async (req, res) => {
   await routeQueryF(hereResult.routes[1], db, summaryResult);
   await routeQueryF(hereResult.routes[2], db, summaryResult);
 
+  /*
+  INSERT INTO route_contained_points (uag_id, route_id)
+  select ua.id as uag_id, r.id as route_id from gis.summaries s
+  join gis.routes r on s.id=r.summary_id
+  join gis.uag ua on ST_CONTAINS(ST_Buffer(r.geom, 20, 'endcap=round join=round'), ua.geom)
+  group by ua.id, r.id;
+  */
+
+  const qb = await db.withSchema('gis')
+  .into(db.raw('gis.route_contained_points(uag_id, route_id)'))
+  .insert(function() {
+    this.select('uag.id', 'routes.id')
+    .from('gis.summaries')
+    .join('gis.routes', 'summaries.id','routes.summary_id')
+    .join(db.raw('gis.uag on ST_CONTAINS(ST_Buffer(routes.geom, 20, \'endcap=round join=round\'), uag.geom)'))
+    .where('summaries.id', summaryResult[0].id.toString())
+    .groupByRaw('uag.id, routes.id')
+    
+  });
+  console.log(qb.toString());
+
   res.send({ time });
 });
 
 async function routeQueryF(route, db, summaryResult) {
   const st = knexPostgis(db);
-
+  
   const sql1 = await db
     .withSchema("gis")
     .insert({
       here_id: route.sections[0].id,
-      geom: st.geomFromText(
-        `LINESTRING(${polylineUtil
-          .decode(route.sections[0].polyline)
-          .polyline.map((i) => i[1] + ` ` + i[0])
-          .join(`, `)
-          .toString()})`,
-        4326
+      geom: st.transform(
+        st.geomFromText(
+          `LINESTRING(${polylineUtil
+            .decode(route.sections[0].polyline)
+            .polyline.map((i) => i[1] + ` ` + i[0])
+            .join(`, `)
+            .toString()})`,
+          4326
+        ), 
+        25832
       ),
       duration: route.sections[0].travelSummary.duration,
       length: route.sections[0].travelSummary.length,
