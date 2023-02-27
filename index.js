a150b41e5ccb70faa07eddbeff862d6ecf4a6b58
JohnSorensenKEA <70580292+JohnSorensenKEA@users.noreply.github.com>
2023-02-27 11:01:57 +0100

    api name and response change + cleanup

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-//const { Client } = require("pg");
-//const Pool = require("pg").Pool;
-//const { Sequelize } = require("sequelize");
 const knex = require("knex");
 const knexPostgis = require("knex-postgis");
 const dotenv = require("dotenv");
@@ -11,47 +8,7 @@ const polylineUtil = require("../TVtS_midlertidig_node_backend/flexible_polyline
 const dbConfig = require("../TVtS_midlertidig_node_backend/db.config.js");
 dotenv.config();
 
-app.get("/ap", async (req, res) => {
-  try {
-    const client = new Client({
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      password: process.env.PGPASSWORD,
-      port: process.env.PGPORT,
-    });
-
-    await client.connect();
-    const result = await client.query("SELECT * FROM gis.accident_points");
-    res.send({ result });
-    await client.end();
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-app.get("/here", async (req, res) => {
-  let result = {};
-  await fetch(
-    `https://router.hereapi.com/v8/routes?transportMode=pedestrian&origin=55.39228,10.38953&destination=55.39497,10.38106&return=travelSummary,polyline&apikey=UmcH7dm4kGVIOe75j3xIX9SHiN_vLRVn-T1INKq-A-g&units=metric&alternatives=2`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      result = data;
-    });
-  //console.log(result.routes[0].sections[0].polyline);
-  //console.log(polylineUtil.decode(result.routes[0].sections[0].polyline));
-
-  const pri = polylineUtil
-    .decode(result.routes[0].sections[0].polyline)
-    .polyline.map((i) => i[1] + ` ` + i[0])
-    .join(`, `);
-  console.log(pri);
-
-  res.send({ result });
-});
-
-app.post("/here", async (req, res) => {
+app.post("/api/generateRoute", async (req, res) => {
   const time = Date.now();
   let hereResult = {};
   await fetch(
@@ -81,61 +38,9 @@ app.post("/here", async (req, res) => {
     .into("summaries")
     .returning("id");
 
-  //console.log(summaryResult[0].id);
-
-  //const sql2 = db.select('id', st.asText('geom')).from('gis.accident_points');//.toString();
-  //console.log(sql2);
-
-  /*
-    //DB test
-    db.raw("SELECT * from gis.accident_points").then(response => {
-      console.log("PostgreSQL connected");
-      console.log(response);
-  })
-  .catch((e) => {
-      console.log("PostgreSQL not connected");
-      console.error(e);
-  });*/
-
-  /*
-    try {
-        const client = new Client({
-          user: process.env.PGUSER,
-          host: process.env.PGHOST,
-          database: process.env.PGDATABASE,
-          password: process.env.PGPASSWORD,
-          port: process.env.PGPORT,
-        });
-
-        const summaryQuery = "INSERT INTO gis.summaries(name) VALUES($1) RETURNING *";
-        const summaryValues = [req.body.name];
-
-        
-        await client.connect();
-        const summaryResult = await client.query(summaryQuery, summaryValues);
-
-        await routeQueryF(hereResult.routes[0], client, summaryResult);
-        await routeQueryF(hereResult.routes[1], client, summaryResult);
-        await routeQueryF(hereResult.routes[2], client, summaryResult);
-
-        
-        await client.end();
-
-      } catch (error) {
-        console.log(error);
-      }*/
-
   await routeQueryF(hereResult.routes[0], db, summaryResult);
   await routeQueryF(hereResult.routes[1], db, summaryResult);
   await routeQueryF(hereResult.routes[2], db, summaryResult);
-
-  /*
-  INSERT INTO route_contained_points (uag_id, route_id)
-  select ua.id as uag_id, r.id as route_id from gis.summaries s
-  join gis.routes r on s.id=r.summary_id
-  join gis.uag ua on ST_CONTAINS(ST_Buffer(r.geom, 20, 'endcap=round join=round'), ua.geom)
-  group by ua.id, r.id;
-  */
 
   const qb = await db.withSchema('gis')
   .into(db.raw('gis.route_contained_points(uag_id, route_id)'))
@@ -150,7 +55,9 @@ app.post("/here", async (req, res) => {
   });
   console.log(qb.toString());
 
-  res.send({ time });
+  const response = summaryResult[0].id.toString();
+
+  res.send({ response });
 });
 
 async function routeQueryF(route, db, summaryResult) {
@@ -177,41 +84,6 @@ async function routeQueryF(route, db, summaryResult) {
       summary_id: summaryResult[0].id,
     })
     .into("routes");
-
-  //console.log(sql1);
-
-  /*
-    const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect/*,
-        operatorsAliases: false,
-      
-        pool: {
-          max: dbConfig.pool.max,
-          min: dbConfig.pool.min,
-          acquire: dbConfig.pool.acquire,
-          idle: dbConfig.pool.idle
-        }
-      });
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }*/
-  /*
-    const routeValues = [
-        route.sections[0].id,
-        polylineUtil.decode(route.sections[0].polyline).polyline.map(i => i[1] + ` ` + i[0]).join(`, `).toString(),
-        route.sections[0].travelSummary.duration,
-        route.sections[0].travelSummary.length,
-        route.sections[0].type,
-        summaryResult.rows[0].id
-    ];
-    //console.log(routeValues); // ST_GeomFromText('LINESTRING( , 4326)')
-    const routeQuery = "INSERT INTO gis.routes(here_id, geom, duration, length, mode, summary_id) VALUES($1, ST_GeomFromText('LINESTRING ($2)', 4326), $3, $4, $5, $6)";
-    await client.query(routeQuery, routeValues);
-    */
 }
 
 const PORT = process.env.PORT || 8080;
